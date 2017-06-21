0ce4e8356856d8347937a25a59782d7e660f19e9
Gordan Savicic <gordo@yugo.at>
2017-06-21 18:25:08 +0200

    removed ledpin2 from SimpleMorse

--- firmware/SimpleMorse/SimpleMorse.ino
+++ firmware/SimpleMorse/SimpleMorse.ino
@@ -9,13 +9,10 @@
 
 #include <morse.h>
 
-#define PIN_LED1 0
-#define PIN_LED2 1
+#define PIN_LED 0
 #define PIN_SPEAKER 2
 
-LEDMorseSender led1Sender(PIN_LED1);
-//MorseSender *led2Sender;
-
+LEDMorseSender ledSender(PIN_LED1);
 SpeakerMorseSender speakerSender(
   PIN_SPEAKER,
   880,  // tone frequency
@@ -24,32 +21,23 @@ SpeakerMorseSender speakerSender(
 
 void setup()
 {
-  led1Sender.setup();
+  ledSender.setup();
   speakerSender.setup();
 
-//  led2Sender = new LEDMorseSender(PIN_LED2);
-//  led2Sender->setup();
-//  led2Sender->setMessage(String("// digital ideation "));
-//  led2Sender->startSending();
-
-  led1Sender.setMessage(String("// digital ideation "));
+  ledSender.setMessage(String("// digital ideation "));
   speakerSender.setMessage(String("// digital ideation "));
 
-  led1Sender.startSending();
+  ledSender.startSending();
   speakerSender.startSending();
 
 }
 
 void loop()
 {
-  if (!led1Sender.continueSending())  {
-    led1Sender.startSending();
+  if (!ledSender.continueSending())  {
+    ledSender.startSending();
   }
-//  if (!led2Sender->continueSending())  {
-//    led2Sender->startSending();
-//  }
   if (!speakerSender.continueSending())  {
     speakerSender.startSending();
   }
-
 }
